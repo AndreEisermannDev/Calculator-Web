78b1771011147d8f0bd52beb1222e2c9875ecdff
Andre Eisermann <81694834+AndreEisermannDev@users.noreply.github.com>
2022-09-17 23:44:28 +0200

    separated script from index
    moved JS code to separate file to clean up code

---
+++ main.js
@@ -0,0 +1,25 @@
+
+        document.write("main.js");
+
+        function appendOperation(operation){
+            document.getElementById('resultArea').innerHTML += operation;
+        }
+
+        function calculateResult(){
+            let container = document.getElementById('resultArea');
+            let result = eval(container.innerHTML);
+            container.innerHTML = result;
+        }
+
+        function deleteLast(operation){
+            let container = document.getElementById('resultArea');
+            if(container.innerHTML.endsWith(' ')){
+            container.innerHTML = container.innerHTML.slice(0, -3)
+            } else {
+                container.innerHTML = container.innerHTML.slice(0, -1)
+            }
+        }
+
+        function resetButton(){
+            document.getElementById('resultArea').innerHTML = '';
+        }
